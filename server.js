2a39c2599b3be97679d64594fbfb004f22907f93
Sabina Liljeström <sabina.liljestrom@hotmail.com>
2024-08-16 16:28:43 +0200

    har skapat tabell för att lagra kurser och felmeddelande om inte alla fält är i fyllda

--- server.js
+++ server.js
@@ -4,33 +4,57 @@
  */
 
 const express = require("express");
-const bodyParser = require ("body-parser");
-const sqlite3 = require ("sqlite3").verbose();
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
 
-//anslut till databas
+// anslut till databas
 const db = new sqlite3.Database("./db/cv.db");
 
-//inställningar
+// inställningar
 const app = express();
 const port = 3000;
-app.set ("view engine", "ejs");
-app.use (express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//routing
-app.get ("/", (req, res) => {
+// routing
+app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.get("/add-course", (req, res) => {
-    res.render("add-course");
+    // Skicka med error som tom sträng när sidan först laddas
+    res.render("add-course", {
+        error: ""
+    });
 });
 
 app.get("/about", (req, res) => {
     res.render("about");
 });
 
-//starta applikationen
-app.listen(port, ()=>{
-    console.log("application stated on port: "+port);
+// Lägg till kurs
+app.post("/add-course", (req, res) => {
+    let coursecode = req.body.coursecode;
+    let coursename = req.body.coursename;
+    let syllabus = req.body.syllabus;
+    let progression = req.body.progression;
+    let error = "";
+
+    // Kontrollera input
+    if (coursecode !== "" && coursename !== "" && syllabus !== "" && progression !== "") {
+        
+    } else {
+        error = "Du måste fylla i alla fält";
+    }
+
+    // Rendera sidan igen med eventuellt felmeddelande
+    res.render("add-course", {
+        error: error
+    });
+});
+
+// Starta applikationen
+app.listen(port, () => {
+    console.log("Application started on port: " + port);
 });
